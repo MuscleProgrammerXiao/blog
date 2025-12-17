d6d42b68a50525cd98b40971834abcc006e2891f
superflashcx <895422334@qq.com>
2025-12-17 18:59:06 +0800

    feat:socialinteraction

---
+++ data/socialInteraction.ts
@@ -0,0 +1,35 @@
+export type SocialUrl = {
+  github: string;
+  x: string;
+  bilibili: string;
+  email: string;
+};
+export type SocialMedia ={
+  title: string;
+  url?: string;
+}
+const socialUrl: SocialUrl = {
+  github: "https://github.com/MuscleProgrammerXiao",
+  x: "https://x.com/docusaurus",
+  bilibili: "https://space.bilibili.com/62832413",
+  email: "mailto:895422334@qq.com",
+};
+const socialMediaData: Record<keyof SocialUrl, SocialMedia>= {
+  github: {
+    title: "GitHub",
+    url: socialUrl.github,
+  },
+  bilibili: {
+    title: "Bilibili",
+    url: socialUrl.bilibili,
+  },
+  x: {
+    title: "X",
+    url: socialUrl.x,
+  },
+  email: {
+    title: "Email",
+    url: socialUrl.email,
+  }
+};
+export default socialMediaData;
